da90d54ed5f65f4280200b034aa69247109f092a
Natalia Panfilenka <choco-cat@mail.u>
2022-08-29 15:50:05 +0300

    init: create structure

---
+++ classes/Permutation.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Permutation class
+ */
+
+namespace classes;
+
+use lib\jsonDB;
+
+class Permutation
+{
+
+    public function __construct()
+    {
+    }
+}
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+use classes\Permutation;
+
+define('BASE_PATH', dirname(realpath(__FILE__)) . '/');
+include (BASE_PATH . 'helpers/autoload.php');
+
+new Permutation();
